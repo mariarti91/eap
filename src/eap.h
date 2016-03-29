e1cdb89512ee4490696ef146b21325617b0d28f4
MJ <mariarti91@gmail.com>
2016-03-29 12:36:10 +0600

    added eap package struct

---
+++ src/eap.h
@@ -0,0 +1,22 @@
+#ifndef __EAP_H__
+#define __EAP_H__
+
+#include <stdint.h>
+
+typedef enum
+{
+	EAP_REQUEST = 1,
+	EAP_RESPONSE,
+	EAP_SUCCESS,
+	EAP_FAILURE,
+}eap_code_fild;
+
+typedef struct
+{
+	uint8_t code;
+	uint8_t identifier;
+	uint16_t length;
+	uint8_t *data;
+}eap_package;
+
+#endif//__EAP_H__
