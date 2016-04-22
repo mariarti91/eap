1268e398738cfeb8162a4a68e99484c485c2a39b
MJ <mariarti91@gmail.com>
2016-04-22 11:45:44 +0600

    server use libauth.a now

--- serv/main.c
+++ serv/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "eap.h"
+#include "auth.h"
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -12,23 +12,23 @@
 int main()
 {
 	printf("I will became a EAP server\n");
-	eap_package eapPack;
+	eap_request eapPack;
 	eapPack.code = EAP_REQUEST;
 	eapPack.identifier = 0xee;
 	eapPack.length = 8;
-	eapPack.data = malloc(4);
-	memset(eapPack.data, 0xdd, 4);
+	eapPack.type = 0xcc;
+	eapPack.type_data = malloc(3);
+	memset(eapPack.type_data, 0xdd, 3);
 
-	eap_package* response = eapExchange(&eapPack);
+	eap_request *res = auth_round(&eapPack);
 
-	printf("Response code = %02x\n", response->code);
-	printf("Response identifier = %02x\n", response->identifier);
-	printf("Response package length = %04x\n", response->length);
+	printf("Response code = %02x\n", res->code);
+	printf("Response identifier = %02x\n", res->identifier);
+	printf("Response package length = %04x\n", res->length);
+	printf("Response type = %02x\n", res->type);
 	printf("Response payload: ");
-	for(int i = 0; i < response->length - 4; printf("%02x", response->data[i++]));
+	for(int i = 0; i < res->length - 5; printf("%02x", res->type_data[i++]));
 	printf("\n");
 	
-	free(response);
-
 	return 0;
 }
