91684a2ae88131622d3635cf064c8c776584c30d
MJ <mariarti91@gmail.com>
2016-04-26 12:14:36 +0600

    eap_peer used libauth now

--- peer/main.c
+++ peer/main.c
@@ -7,6 +7,7 @@
 #include <sys/time.h>
 #include <stdint.h>
 
+#include "auth.h"
 #include "eap.h"
 #include "lower.h"
 
@@ -15,12 +16,13 @@ int main()
 	printf("I will became a EAP peer\n");
 
 	startListener(9090);
-	eap_package *pack = getEapPackage();
+	eap_request *pack = getEapRequest();
 	printf("EAP package code = %02x\n", pack->code);
 	printf("EAP package identifier = %02x\n", pack->identifier);
 	printf("EAP package length = %04x\n", pack->length);
-	for(int i = 0; i < pack->length - 4; printf("%02x", pack->data[i++]));
+	printf("EAP package type = %02x\n", pack->type);
+	for(int i = 0; i < pack->length - 5; printf("%02x", pack->type_data[i++]));
 	printf("\n");
-	sendEapPackage(pack);
+	sendEapRequest(pack);
 	return 0;
 }
