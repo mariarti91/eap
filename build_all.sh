ed7b0599f3b3e77673aba221c631de3bc43b5342
MJ <nds@csp.tusur.ru>
2016-05-04 11:03:50 +0600

    added path for auth_eap into build_all script

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd libs/lower && ./build.sh && cd ..
-cd eap && ./build.sh && cd ../..
+cd eap && ./build.sh && cd ..
+cd auth_eap && ./build.sh && cd ../..
 
 cd serv && ./build.sh && cd ..
 cd peer && ./build.sh && cd ..
