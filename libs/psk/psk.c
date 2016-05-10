2523de05a4c852828f4bca49fe921fc272035adc
MJ <nds@csp.tusur.ru>
2016-05-10 11:09:01 +0600

    into psk library added waitAuth function

--- libs/psk/psk.c
+++ libs/psk/psk.c
@@ -1,1 +1,24 @@
 #include "psk.h"
+
+#include <stdio.h>
+
+#include "auth.h"
+#include "lower.h"
+
+
+int startAuth()
+{
+}
+
+void waitAuth()
+{
+	startListener(9090);
+	eap_request *pack = getEapRequest();
+	printf("EAP package code = %02x\n", pack->code);
+	printf("EAP package identifier = %02x\n", pack->identifier);
+	printf("EAP package length = %04x\n", pack->length);
+	printf("EAP package type = %02x\n", pack->type);
+	for(int i = 0; i < pack->length - 5; printf("%02x", pack->type_data[i++]));
+	printf("\n");
+	sendEapRequest(pack);
+}
--- libs/psk/psk.h
+++ libs/psk/psk.h
@@ -1,6 +1,7 @@
 #ifndef __EAP_PSK_H__
 #define __EAP_PSK_H__
 
-
+int startAuth();
+void waitAuth();
 
 #endif//__EAP_PSK_H__
