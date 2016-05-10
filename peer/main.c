f2e86b68ebf72773d62b226595da3e7d2aceef8f
MJ <nds@csp.tusur.ru>
2016-05-10 11:11:30 +0600

    peer used psk library now

--- peer/main.c
+++ peer/main.c
@@ -1,28 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <stdint.h>
-
-#include "auth.h"
-#include "eap.h"
-#include "lower.h"
+#include "psk.h"
 
 int main()
 {
 	printf("I will became a EAP peer\n");
-
-	startListener(9090);
-	eap_request *pack = getEapRequest();
-	printf("EAP package code = %02x\n", pack->code);
-	printf("EAP package identifier = %02x\n", pack->identifier);
-	printf("EAP package length = %04x\n", pack->length);
-	printf("EAP package type = %02x\n", pack->type);
-	for(int i = 0; i < pack->length - 5; printf("%02x", pack->type_data[i++]));
-	printf("\n");
-	sendEapRequest(pack);
+	waitAuth();
 	return 0;
 }
