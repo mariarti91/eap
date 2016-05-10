0873fd13482484909ebd417e0014ebc6052b20bb
MJ <nds@csp.tusur.ru>
2016-05-10 16:03:24 +0600

    first EAP-PSK message

--- libs/psk/psk.c
+++ libs/psk/psk.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 
 #include "auth.h"
 #include "lower.h"
@@ -12,6 +13,8 @@
 uint8_t PSK[16] = {0xdd};
 uint8_t AK[16] = {0};
 uint8_t KDK[16] = {0};
+
+uint8_t identifier = 0;
 
 void keysSettings()
 {
@@ -22,25 +25,47 @@ void keysSettings()
 	encript_block(c0, buf, w);
 	uint8_t c1[16] = {0};
 	c1[15] = 0x01;
-	for (int i = 0; i < 15; i++)
+	for (int i = 0; i < 16; i++)
 		c1[i] = c1[i] ^ buf[i];
 	encript_block(c1, AK, w);
 	uint8_t c2[16] = {0};
 	c2[15] = 0x02;
-	for(int i = 0; i < 15; i++)
+	for(int i = 0; i < 16; i++)
 		c2[i] = c2[i] ^ buf[i];
 	encript_block(c2, KDK, w);
 }
 
 int startAuth()
 {
+	keysSettings();
+	srand(time(NULL));
+	identifier = rand() % 0x100;
+	uint8_t rand_s[16];
+	for(int i = 0; i < 16; rand_s[i++] = rand() % 0x100);
+	printf("RAND_S: ");
+	for(int i = 0; i < 16; printf("%02x", rand_s[i++]));
+	printf("\n");
+	uint8_t flags = 0x00;
+	uint8_t id_s = 0x11;
 	eap_request eapPack;
 	eapPack.code = EAP_REQUEST;
-	eapPack.identifier = 0xee;
-	eapPack.length = 8;
-	eapPack.type = 0xcc;
-	eapPack.type_data = malloc(3);
-	memset(eapPack.type_data, 0xdd, 3);
+	eapPack.identifier = identifier++;
+	eapPack.length = 23;
+	eapPack.type = 0xff;
+	eapPack.type_data = malloc(18);
+	eapPack.type_data[0] = flags;
+	memcpy(&eapPack.type_data[1], &rand_s[0], 16);
+	eapPack.type_data[17] = id_s;
+
+	printf("FIRST REQUEST:\n");
+	printf("Request code = %02x\n", eapPack.code);
+	printf("Request identifier = %02x\n", eapPack.identifier);
+	printf("Request length = %04x\n", eapPack.length);
+	printf("Request type = %02x\n", eapPack.type);
+	printf("Reqyest type data: ");
+	for(int i = 0; i < eapPack.length - 5; printf("%02x", eapPack.type_data[i++]));
+	printf("\nEND\n");
+
 
 	eap_request *res = auth_round(&eapPack);
 
@@ -56,6 +81,7 @@ int startAuth()
 
 void waitAuth()
 {
+	keysSettings();
 	startListener(9090);
 	eap_request *pack = getEapRequest();
 	printf("EAP package code = %02x\n", pack->code);
